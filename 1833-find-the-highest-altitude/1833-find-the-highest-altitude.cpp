e900112669512a06138352eb9c7d2e184fee513e
Nikhil Kumar Das <72371791+NikhilKD@users.noreply.github.com>
2023-06-19 14:21:42 +0530

    Time: 6 ms (28.46%), Space: 8.2 MB (8.24%) - LeetHub

--- 1833-find-the-highest-altitude/1833-find-the-highest-altitude.cpp
+++ 1833-find-the-highest-altitude/1833-find-the-highest-altitude.cpp
@@ -3,9 +3,11 @@ public:
     int largestAltitude(vector<int>& gain) {
         int n=gain.size();
         vector<int> p(n+1,0);
+        int maxi=0;
         for(int i=0;i<n;i++){
             p[i+1]=p[i]+gain[i];
+            maxi=max(p[i+1],maxi);
         }
-        return *max_element(p.begin(),p.end());
+        return maxi;
     }
 };
