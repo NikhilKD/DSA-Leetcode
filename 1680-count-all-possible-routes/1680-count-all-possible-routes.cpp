5ffa343092b237a19a204b52655c9030995f570e
Nikhil Kumar Das <72371791+NikhilKD@users.noreply.github.com>
2023-06-29 18:14:17 +0530

    Time: 209 ms (70.88%), Space: 12 MB (43.1%) - LeetHub

--- 1680-count-all-possible-routes/1680-count-all-possible-routes.cpp
+++ 1680-count-all-possible-routes/1680-count-all-possible-routes.cpp
@@ -1,23 +1,28 @@
 class Solution {
 public:
-    int dp[101][201];
-
-    int getans(int i, int fuel, int finish, vector<int>& loc){
-        if(fuel < 0) return 0;
-        if(dp[i][fuel] != -1) return dp[i][fuel];
-
-        int ans = 0;
-        if(i == finish) ans++;
-        
-        for(int j = 0; j < loc.size(); j++){
-            if(i != j) ans = (ans + getans(j, fuel - abs(loc[i] - loc[j]), finish, loc)) % 1000000007;
+    int N=1e9+7;
+    int f(int i,int n,vector<int>& loc,int fuel,vector<vector<int>> &dp){
+        long long ans=0;
+        if(fuel<0){
+            return 0;
         }
-        return dp[i][fuel] = ans;
+        if(i==n){
+            ans++;
+        }
+        if(dp[i][fuel]!=-1){
+            return dp[i][fuel];
+        }
+        for(int j=0;j<loc.size();j++){
+            int x=abs(loc[j]-loc[i]);
+            if(j!=i){
+                ans+=f(j,n,loc,fuel-x,dp)%N;
+            }
+        }
+        return dp[i][fuel]=ans%N;
     }
-
-    int countRoutes(vector<int>& locations, int start, int finish, int fuel) {
-        memset(dp, -1, sizeof dp);
-        return getans(start, fuel, finish, locations);
-
+    int countRoutes(vector<int>& loc, int start, int finish, int fuel) {
+        vector<vector<int>> dp(loc.size()+1,vector<int> (fuel+1,-1));
+        return f(start,finish,loc,fuel,dp);
     }
 };
+
